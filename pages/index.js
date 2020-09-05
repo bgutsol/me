437aa5d8dda4d75ca95466d6b83084f8f34c1be4
bgutsol <bogdan.gutsol@gmail.com>
2020-09-06 02:30:31 +0300

    init

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,97 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export const config = {
+  unstable_runtimeJS: false,
+};
 
 export default function Home() {
+  // document.querySelector("#theme-btn").addEventListener("click", () => {
+  //   var themes = [
+  //     "default",
+  //     "dark",
+  //     "red",
+  //     "blue",
+  //     "yellow",
+  //     "green",
+  //     "violet",
+  //   ];
+  //   var dataTheme = document.documentElement.dataset.theme;
+  //   themes = themes.filter((theme) => theme !== dataTheme);
+  //   document.documentElement.dataset.theme =
+  //     themes[Math.floor(Math.random() * themes.length)];
+  // });
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Bohdan Hutsol</title>
+        <meta
+          name="description"
+          content="Hey! I’m Bohdan Hutsol, a front-end engineer."
+        />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏄🏼‍♂️</text></svg>"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@500&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <button id="theme-btn" className={styles.themeBtn}>
+          Change theme
+        </button>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.description}>
+          <p>Hey! I’m Bohdan.</p>
+          <p>I’m front-end engineer with a passion for design.</p>
+          <p>
+            I’m always in searching for perfection, clarity and simplicity in
+            objects and I aim for it in my work.
+          </p>
         </div>
+
+        <ul className={styles.links}>
+          <li className={styles.linkItem}>
+            <a href="mailto:bogdan.gutsol@gmail.com" target="_blank">
+              email
+            </a>
+          </li>
+          <li className={styles.linkItem}>
+            <a
+              href="https://www.linkedin.com/in/bohdan-hutsol/"
+              target="_blank"
+            >
+              linkedin
+            </a>
+          </li>
+          <li className={styles.linkItem}>
+            <a href="https://github.com/bgutsol" target="_blank">
+              github
+            </a>
+          </li>
+          <li className={styles.linkItem}>
+            <a href="https://twitter.com/bgutsol" target="_blank">
+              twitter
+            </a>
+          </li>
+          <li className={styles.linkItem}>
+            <a href="/CV_BohdanHutsol.pdf" target="_blank">
+              CV
+            </a>
+          </li>
+        </ul>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <script
+        dangerouslySetInnerHTML={{
+          __html:
+            'document.querySelector("#theme-btn").addEventListener("click", () => { var themes = [ "default", "dark", "red", "blue", "yellow", "green", "violet", ]; var dataTheme = document.documentElement.dataset.theme; themes = themes.filter((theme) => theme !== dataTheme); document.documentElement.dataset.theme = themes[Math.floor(Math.random() * themes.length)]; });',
+        }}
+      ></script>
     </div>
-  )
+  );
 }
